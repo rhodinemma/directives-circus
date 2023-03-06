e9177b06baf23585f7b882340427e93c46178aa4
rhodinemma <rhodinemma10@gmail.com>
2023-03-06 08:50:10 +0300

    feat: review ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directives-circus';
+  images = [
+    {
+      title: 'At the Beach',
+      url:
+        'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60'
+    },
+    {
+      title: 'At the Forest',
+      url:
+        'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60'
+    },
+    {
+      title: 'At the City',
+      url:
+        'https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60'
+    },
+    {
+      title: 'At the Snow',
+      url:
+        'https://images.unsplash.com/photo-1517299321609-52687d1bc55a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60'
+    },
+  ];
 }
