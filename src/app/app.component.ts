283ddf2f486967537313c074df41f46d8986dba9
rhodinemma <rhodinemma10@gmail.com>
2023-03-06 14:27:39 +0300

    feat: the NgClass directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentPage = 0;
   images = [
     {
       title: 'At the Beach',
