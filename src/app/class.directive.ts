518825979607dfd4664c79814be3f6f7b54144b0
rhodinemma <rhodinemma10@gmail.com>
2023-03-20 13:03:18 +0300

    feat: intercepting a property assignment

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,13 +1,16 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
-
   constructor(private element: ElementRef) {
     //console.log("Class directive used")
-    this.element.nativeElement.style.backgroundColor = 'red';
+    // this.element.nativeElement.style.backgroundColor = this.backgroundColor;
+  }
+
+  @Input() set backgroundColor(color: string) {
+    this.element.nativeElement.style.backgroundColor = color;
   }
 
 }
