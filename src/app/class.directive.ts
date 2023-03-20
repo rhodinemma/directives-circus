30c0abd9654a6b895741698490d0ae18b4961bf9
rhodinemma <rhodinemma10@gmail.com>
2023-03-20 13:07:30 +0300

    feat: input aliasing in directives

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -9,7 +9,7 @@ export class ClassDirective {
     // this.element.nativeElement.style.backgroundColor = this.backgroundColor;
   }
 
-  @Input() set backgroundColor(color: string) {
+  @Input('appClass') set backgroundColor(color: string) {
     this.element.nativeElement.style.backgroundColor = color;
   }
 
