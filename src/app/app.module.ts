50bde985e833c446ab2421367070bed14c854600
rhodinemma <rhodinemma10@gmail.com>
2023-03-20 15:00:15 +0300

    feat: custom structural directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ClassDirective } from './class.directive';
+import { TimesDirective } from './times.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClassDirective
+    ClassDirective,
+    TimesDirective
   ],
   imports: [
     BrowserModule
